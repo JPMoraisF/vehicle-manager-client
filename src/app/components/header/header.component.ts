b885f50e00e826e9ea10837ff23d449a41ac1788
João Pedro de Morais Ferreira <jpmferreira96@gmail.com>
2025-01-24 11:48:13 -0300

    Minor fixes and changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -11,7 +12,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription;
   isAuthenticated: boolean = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.userSub = this.authService.user.subscribe(user => {
@@ -25,5 +26,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   logout(): void {
     this.authService.logout();
+    this.router.navigate(['/']);
+
   }
 }
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
-
+  easyToUseText = 'Simple set up and configuration ensures that user won\'t have to deal with tedious setup processes and long training sessions.'
+  fleetManagement = 'Manage your fleet from anywhere in the world. With cloud-based infrastructure you can at a glance observe your fleet maintenance history and driver information.'
+  costCenter = 'With our new Cost Center menu, you can see your profits and expenses in detailed information and export to multiple formats for data analysis.'
 }
